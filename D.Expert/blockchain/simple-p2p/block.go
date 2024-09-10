58d8d2ec1ac5b6ecc2c00c44d66ebcfaff830f40
Yudhistiro Tri Aronggo <yudhistiro.aronggo@btpnsyariah.com>
2024-09-10 15:36:22 +0700

    comment p2p simply

--- D.Expert/blockchain/simple-p2p/block.go
+++ D.Expert/blockchain/simple-p2p/block.go
@@ -1,37 +1,29 @@
 package main
 
-import (
-	"crypto/sha256"
-	"encoding/json"
-	"fmt"
-	"strconv"
-	"strings"
-)
-
-type BlockData struct {
-	Location   string
-	WaveHeight int
-}
-
-type Block struct {
-	Data         BlockData
-	Hash         string
-	PreviousHash string
-	Timestamp    int64
-	Height       int
-	Pow          int
-}
-
-func (b Block) calculateHash() string {
-	data, _ := json.Marshal(b.Data)
-	blockData := b.PreviousHash + string(data) + strconv.FormatInt(b.Timestamp, 10) + strconv.Itoa(b.Height) + strconv.Itoa(b.Pow)
-	blockHash := sha256.Sum256([]byte(blockData))
-	return fmt.Sprintf("%x", blockHash)
-}
-
-func (b *Block) mine(difficulty int) {
-	for !strings.HasPrefix(b.Hash, strings.Repeat("0", difficulty)) {
-		b.Pow++
-		b.Hash = b.calculateHash()
-	}
-}
+//type BlockData struct {
+//	Location   string
+//	WaveHeight int
+//}
+//
+//type Block struct {
+//	Data         BlockData
+//	Hash         string
+//	PreviousHash string
+//	Timestamp    int64
+//	Height       int
+//	Pow          int
+//}
+//
+//func (b Block) calculateHash() string {
+//	data, _ := json.Marshal(b.Data)
+//	blockData := b.PreviousHash + string(data) + strconv.FormatInt(b.Timestamp, 10) + strconv.Itoa(b.Height) + strconv.Itoa(b.Pow)
+//	blockHash := sha256.Sum256([]byte(blockData))
+//	return fmt.Sprintf("%x", blockHash)
+//}
+//
+//func (b *Block) mine(difficulty int) {
+//	for !strings.HasPrefix(b.Hash, strings.Repeat("0", difficulty)) {
+//		b.Pow++
+//		b.Hash = b.calculateHash()
+//	}
+//}
--- D.Expert/blockchain/simple-p2p/main.go
+++ D.Expert/blockchain/simple-p2p/main.go
@@ -1,72 +1,60 @@
 package main
 
-import (
-	"context"
-	"crypto/rand"
-	"flag"
-	"fmt"
-	"io"
-	"log"
-	mrand "math/rand"
-	"os"
-	"sync"
-)
-
-var mutex = &sync.Mutex{}
-var mychain = NewBlockchain(3)
-
-func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	sourcePort := flag.Int("sp", 0, "Source port number")
-	dest := flag.String("d", "", "Destination multiaddr string")
-	help := flag.Bool("help", false, "Display help")
-	debug := flag.Bool("debug", false, "Debug generates the same node ID on every execution")
-
-	flag.Parse()
-
-	if *help {
-		fmt.Printf("This program demonstrates a simple p2p blockchain application\n\n")
-		fmt.Println("Usage: Run './simple-blockchain -sp <SOURCE_PORT>' where <SOURCE_PORT> can be any port number.")
-		fmt.Println("Now run './simple-blockchain -d <MULTIADDR>' where <MULTIADDR> is multiaddress of previous listener host.")
-
-		os.Exit(0)
-	}
-
-	// If debug is enabled, use a constant random source to generate the peer ID. Only useful for debugging,
-	// off by default. Otherwise, it uses rand.Reader.
-	var r io.Reader
-	if *debug {
-		// Use the port number as the randomness source.
-		// This will always generate the same host ID on multiple executions, if the same port number is used.
-		// Never do this in production code.
-		r = mrand.New(mrand.NewSource(int64(*sourcePort)))
-	} else {
-		r = rand.Reader
-	}
-
-	h, err := makeHost(*sourcePort, r)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	if *dest == "" {
-		startPeer(ctx, h, handleStream)
-	} else {
-		rw, err := startPeerAndConnect(ctx, h, *dest)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		// Create a thread to read and write data.
-		go writeData(rw)
-		go readData(rw)
-
-	}
-
-	// Wait forever
-	select {}
-}
+//var mutex = &sync.Mutex{}
+//var mychain = NewBlockchain(3)
+//
+//func main() {
+//	ctx, cancel := context.WithCancel(context.Background())
+//	defer cancel()
+//
+//	sourcePort := flag.Int("sp", 0, "Source port number")
+//	dest := flag.String("d", "", "Destination multiaddr string")
+//	help := flag.Bool("help", false, "Display help")
+//	debug := flag.Bool("debug", false, "Debug generates the same node ID on every execution")
+//
+//	flag.Parse()
+//
+//	if *help {
+//		fmt.Printf("This program demonstrates a simple p2p blockchain application\n\n")
+//		fmt.Println("Usage: Run './simple-blockchain -sp <SOURCE_PORT>' where <SOURCE_PORT> can be any port number.")
+//		fmt.Println("Now run './simple-blockchain -d <MULTIADDR>' where <MULTIADDR> is multiaddress of previous listener host.")
+//
+//		os.Exit(0)
+//	}
+//
+//	// If debug is enabled, use a constant random source to generate the peer ID. Only useful for debugging,
+//	// off by default. Otherwise, it uses rand.Reader.
+//	var r io.Reader
+//	if *debug {
+//		// Use the port number as the randomness source.
+//		// This will always generate the same host ID on multiple executions, if the same port number is used.
+//		// Never do this in production code.
+//		r = mrand.New(mrand.NewSource(int64(*sourcePort)))
+//	} else {
+//		r = rand.Reader
+//	}
+//
+//	h, err := makeHost(*sourcePort, r)
+//	if err != nil {
+//		log.Println(err)
+//		return
+//	}
+//
+//	if *dest == "" {
+//		startPeer(ctx, h, handleStream)
+//	} else {
+//		rw, err := startPeerAndConnect(ctx, h, *dest)
+//		if err != nil {
+//			log.Println(err)
+//			return
+//		}
+//
+//		// Create a thread to read and write data.
+//		go writeData(rw)
+//		go readData(rw)
+//
+//	}
+//
+//	// Wait forever
+//	select {}
+//}
